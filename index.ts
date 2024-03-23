c672e5bc460aaa8ce73436183eb26442061a05ae
Huda Noor <160596770+Huda-noor@users.noreply.github.com>
2024-03-24 02:09:31 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNum = Math.floor( Math.random() *10 + 1);
+
+const answer = await inquirer.prompt([{
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guess a number 1-10:",
+
+    
+
+}])
+
+if(answer.userGuessedNumber === randomNum){
+    console.log("CONGRATULATIONS! You guessed right number.You are genius")
+}else{
+    console.log("Wrong! You missed it.");
+}
